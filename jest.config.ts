64be5c30423876bb5013eb7c54baedca58b40496
Dark Light <pazsebas@hotmail.com>
2026-02-03 21:24:05 -0500

    chore: :heavy_plus_sign: add @ngneat/spectator dependency and setup Jest environment for Angular testing

--- jest.config.ts
+++ jest.config.ts
@@ -1,11 +1,14 @@
 import type { Config } from 'jest';
 
 export default {
+  preset: 'jest-preset-angular',
+  setupFilesAfterEnv: ['<rootDir>/setup-jest.ts'],
+  testPathIgnorePatterns: ['/node_modules/', '/dist/'],
   moduleNameMapper: {
-    '@shared/(.*)': ['./src/app/domains/shared/$1'],
-    '@products/(.*)': ['./src/app/domains/products/$1'],
-    '@info/(.*)': ['./src/app/domains/info/$1'],
-    '@env/(.*)': ['./src/environments/$1'],
+    '@shared/(.*)': '<rootDir>/src/app/domains/shared/$1',
+    '@products/(.*)': '<rootDir>/src/app/domains/products/$1',
+    '@info/(.*)': '<rootDir>/src/app/domains/info/$1',
+    '@env/(.*)': '<rootDir>/src/environments/$1',
   },
   coverageDirectory: './coverage',
   collectCoverageFrom: [
