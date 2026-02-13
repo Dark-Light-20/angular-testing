435b85d402d1e7c82dc9ed929bec9f16d24163d7
Dark Light <pazsebas@hotmail.com>
2026-02-12 20:14:33 -0500

    test: :white_check_mark: add unit test for related products component in ProductDetailComponent mocking defer behavior

--- src/app/domains/products/pages/product-detail/product-detail.component.spec.ts
+++ src/app/domains/products/pages/product-detail/product-detail.component.spec.ts
@@ -9,6 +9,18 @@ import ProductDetailComponent from './product-detail.component';
 import { ProductService } from '@shared/services/product.service';
 import { generateFakeProduct } from '@shared/models/product.mock';
 import { of } from 'rxjs';
+import { DeferBlockBehavior } from '@angular/core/testing';
+import { RelatedComponent } from '@products/components/related/related.component';
+
+globalThis.IntersectionObserver = jest.fn(() => ({
+  observe: jest.fn(),
+  unobserve: jest.fn(),
+  disconnect: jest.fn(),
+  takeRecords: jest.fn(),
+  root: null,
+  rootMargin: '',
+  thresholds: [],
+}));
 
 describe('ProductDetailComponent', () => {
   let spectator: Spectator<ProductDetailComponent>;
@@ -18,6 +30,7 @@ describe('ProductDetailComponent', () => {
 
   const createComponent = createComponentFactory({
     component: ProductDetailComponent,
+    deferBlockBehavior: DeferBlockBehavior.Manual,
     providers: [
       mockProvider(ProductService, {
         getOneBySlug: jest.fn().mockReturnValue(of(mockProduct)),
@@ -48,5 +61,14 @@ describe('ProductDetailComponent', () => {
     const coverImg = spectator.query<HTMLImageElement>(byTestId('cover'));
     expect(coverImg).toBeTruthy();
     expect(coverImg?.src).toContain(mockProduct.images[0]);
+  });
+
+  it('should show related products component', async () => {
+    spectator.detectChanges();
+    await spectator.deferBlock().renderComplete();
+    spectator.detectChanges();
+
+    const relatedComponent = spectator.query(RelatedComponent);
+    expect(relatedComponent).toBeTruthy();
   });
 });
